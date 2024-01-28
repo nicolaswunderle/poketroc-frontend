7d0a41f528199807778ddcc55d2876a3b90eb791
Nicolas Wunderle <nicolas.wunderle@heig-vd.ch>
2024-01-28 23:46:37 +0100

    modification du dresseur

---
+++ src/app/layout/profil/profil.model.ts
@@ -0,0 +1,9 @@
+export type Profil = {
+    prenom: string;
+    nom: string;
+    pseudo: string;
+    date_naissance: Date;
+    email: string;
+    deck_visible: boolean;
+    mot_de_passe: string;
+};
